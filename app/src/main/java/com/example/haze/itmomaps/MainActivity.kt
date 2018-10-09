4b57dc0dc6d6cc35eecf87a0be75dfffea7c44c9
Alexey <gusevalexey0@gmail.com>
2018-10-09 21:57:17 +0300

    change design add context menu spawning on long tap sav state of main activity on changing landscape

--- app/src/main/java/com/example/haze/itmomaps/MainActivity.kt
+++ app/src/main/java/com/example/haze/itmomaps/MainActivity.kt
@@ -3,9 +3,11 @@ package com.example.haze.itmomaps
 import android.content.Intent
 import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.view.Menu
+import android.view.ContextMenu
 import android.view.MenuItem
-import androidx.appcompat.app.ActionBarDrawerToggle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import com.github.chrisbanes.photoview.PhotoView
@@ -17,20 +19,17 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
     private lateinit var floorView: PhotoView
     private lateinit var floorPicker: com.shawnlin.numberpicker.NumberPicker
+    private lateinit var buildingSelector: Spinner
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
         fab.setOnClickListener {
             val intent = Intent(this, RouteActivity::class.java)
             startActivity(intent)
         }
 
-        val toggle = ActionBarDrawerToggle(
-                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
-        drawer_layout.addDrawerListener(toggle)
-        toggle.syncState()
+        val buildingNames = arrayOf("Kronv", "Lomo", "Grivc")
         val currentBuilding = Building("EATMore", arrayOf(BitmapFactory.decodeResource(resources, R.drawable.floor1), BitmapFactory.decodeResource(resources, R.drawable.floor2), BitmapFactory.decodeResource(resources, R.drawable.floor3), BitmapFactory.decodeResource(resources, R.drawable.floor4), BitmapFactory.decodeResource(resources, R.drawable.floor5)), 5)
         floorView = findViewById(R.id.photo_view)
         floorPicker = findViewById(R.id.number_picker)
@@ -38,7 +37,32 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         floorPicker.maxValue = currentBuilding.numberOfFloors
         floorView.setImageBitmap(currentBuilding.floors[floorPicker.value - 1])
         floorPicker.setOnValueChangedListener { _, _, newValue -> floorView.setImageBitmap(currentBuilding.floors[newValue - 1]) }
+        registerForContextMenu(floorView)
+        floorView.setOnLongClickListener { openContextMenu(it); true }
         nav_view.setNavigationItemSelectedListener(this)
+
+        buildingSelector = findViewById(R.id.spinner)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, buildingNames)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        buildingSelector.adapter = adapter
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putInt("currentFloor", floorPicker.value)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if (savedInstanceState != null) {
+            floorPicker.value = savedInstanceState.getInt("currentFloor")
+        }
+    }
+
+    override fun onCreateContextMenu(menu: ContextMenu?, v: View?, menuInfo: ContextMenu.ContextMenuInfo?) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        menuInflater.inflate(R.menu.context_menu, menu)
     }
 
     override fun onBackPressed() {
@@ -46,22 +70,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             drawer_layout.closeDrawer(GravityCompat.START)
         } else {
             super.onBackPressed()
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        when (item.itemId) {
-            R.id.action_settings -> return true
-            else -> return super.onOptionsItemSelected(item)
         }
     }
 
